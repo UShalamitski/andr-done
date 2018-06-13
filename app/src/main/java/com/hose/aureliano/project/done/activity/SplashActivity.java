0fbe666df1905f0458f56b01dff61cddd913b1a1
Uladzislau Shalamitski <Uladzislau_Shalamitski@epam.com>
2018-06-13 18:06:28 +0300

    Add splash activity

---
+++ app/src/main/java/com/hose/aureliano/project/done/activity/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.hose.aureliano.project.done.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Splash activity.
+ * <p>
+ * Date: 6/13/2018.
+ *
+ * @author Uladzislau_Shalamitski
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, ListsActivity.class));
+        finish();
+    }
+}
